df826e35d525b20742d7f8802aa2b3eca4c10056
Andrew <atklenner@gmail.com>
2022-05-30 15:06:49 -0700

    project setup

---
+++ simple-node-project/server.js
@@ -0,0 +1,61 @@
+// const http = require("http");
+// const fs = require("fs");
+// const url = require("url");
+// const querystring = require("querystring");
+// const figlet = require("figlet");
+
+// const server = http.createServer((req, res) => {
+//   const page = url.parse(req.url).pathname;
+//   const params = querystring.parse(url.parse(req.url).query);
+//   console.log(page);
+//   switch (page) {
+//     case "/":
+//       fs.readFile("index.html", function (err, data) {
+//         res.writeHead(200, { "Content-Type": "text/html" });
+//         res.write(data);
+//         res.end();
+//       });
+//       break;
+//     case "/otherpage":
+//       fs.readFile("otherpage.html", function (err, data) {
+//         res.writeHead(200, { "Content-Type": "text/html" });
+//         res.write(data);
+//         res.end();
+//       });
+//       break;
+//     case "/otherotherpage":
+//       fs.readFile("otherotherpage.html", function (err, data) {
+//         res.writeHead(200, { "Content-Type": "text/html" });
+//         res.write(data);
+//         res.end();
+//       });
+//       break;
+//     case "/api":
+//       studentReturn(params, res);
+//       break;
+//     case "/css/style.css":
+//       fs.readFile("css/style.css", function (err, data) {
+//         res.write(data);
+//         res.end();
+//       });
+//       break;
+//     case "/js/main.js":
+//       fs.readFile("js/main.js", function (err, data) {
+//         res.writeHead(200, { "Content-Type": "text/javascript" });
+//         res.write(data);
+//         res.end();
+//       });
+//       break;
+//     default:
+//       figlet("404!!", function (err, data) {
+//         if (err) {
+//           console.log("Something went wrong...");
+//           console.dir(err);
+//           return;
+//         }
+//         res.write(data);
+//         res.end();
+//       });
+//       break;
+//   }
+// });
