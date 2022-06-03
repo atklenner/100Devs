af1fd1041b2b6ae24bdc5b48504aa2f57d5f2ca5
Andrew <atklenner@gmail.com>
2022-06-03 11:45:55 -0700

    finish coin flipper

--- simple-node-project/server.js
+++ simple-node-project/server.js
@@ -1,61 +1,57 @@
-// const http = require("http");
-// const fs = require("fs");
-// const url = require("url");
-// const querystring = require("querystring");
-// const figlet = require("figlet");
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+const querystring = require("querystring");
+const figlet = require("figlet");
 
-// const server = http.createServer((req, res) => {
-//   const page = url.parse(req.url).pathname;
-//   const params = querystring.parse(url.parse(req.url).query);
-//   console.log(page);
-//   switch (page) {
-//     case "/":
-//       fs.readFile("index.html", function (err, data) {
-//         res.writeHead(200, { "Content-Type": "text/html" });
-//         res.write(data);
-//         res.end();
-//       });
-//       break;
-//     case "/otherpage":
-//       fs.readFile("otherpage.html", function (err, data) {
-//         res.writeHead(200, { "Content-Type": "text/html" });
-//         res.write(data);
-//         res.end();
-//       });
-//       break;
-//     case "/otherotherpage":
-//       fs.readFile("otherotherpage.html", function (err, data) {
-//         res.writeHead(200, { "Content-Type": "text/html" });
-//         res.write(data);
-//         res.end();
-//       });
-//       break;
-//     case "/api":
-//       studentReturn(params, res);
-//       break;
-//     case "/css/style.css":
-//       fs.readFile("css/style.css", function (err, data) {
-//         res.write(data);
-//         res.end();
-//       });
-//       break;
-//     case "/js/main.js":
-//       fs.readFile("js/main.js", function (err, data) {
-//         res.writeHead(200, { "Content-Type": "text/javascript" });
-//         res.write(data);
-//         res.end();
-//       });
-//       break;
-//     default:
-//       figlet("404!!", function (err, data) {
-//         if (err) {
-//           console.log("Something went wrong...");
-//           console.dir(err);
-//           return;
-//         }
-//         res.write(data);
-//         res.end();
-//       });
-//       break;
-//   }
-// });
+const server = http.createServer((req, res) => {
+  const page = url.parse(req.url).pathname;
+  const params = querystring.parse(url.parse(req.url).query);
+  console.log(page);
+  switch (page) {
+    case "/":
+      fs.readFile("index.html", function (err, data) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        res.end();
+      });
+      break;
+    case "/flip":
+      res.writeHead(200, { "Content-Type": "application/json" });
+      let json;
+      let flip = Math.floor(Math.random() * 2);
+      if (flip === 0) {
+        json = { flip: "heads" };
+      } else {
+        json = { flip: "tails" };
+      }
+      res.end(JSON.stringify(json));
+      break;
+    case "/css/style.css":
+      fs.readFile("css/style.css", function (err, data) {
+        res.write(data);
+        res.end();
+      });
+      break;
+    case "/js/main.js":
+      fs.readFile("js/main.js", function (err, data) {
+        res.writeHead(200, { "Content-Type": "text/javascript" });
+        res.write(data);
+        res.end();
+      });
+      break;
+    default:
+      figlet("404!!", function (err, data) {
+        if (err) {
+          console.log("Something went wrong...");
+          console.dir(err);
+          return;
+        }
+        res.write(data);
+        res.end();
+      });
+      break;
+  }
+});
+
+server.listen(8000);
