1279422517f6d51022f3f21802557152a78f8269
Andrew <atklenner@gmail.com>
2022-06-04 12:48:38 -0700

    connect db

--- CRUD-tutorial/server.js
+++ CRUD-tutorial/server.js
@@ -1,11 +1,26 @@
+require("dotenv").config();
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+const MongoClient = require("mongodb").MongoClient;
+
+MongoClient.connect(process.env.CONNECTION_STRING)
+  .then((client) => {
+    console.log("Connected to Database");
+    const db = client.db("star-wars-quotes");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/quotes", (req, res) => {
+  console.log(req.body);
   console.log("submit");
 });
 
