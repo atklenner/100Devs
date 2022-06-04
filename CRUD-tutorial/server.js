4901b6f3d373e48d49e21c19ae5b7a5e3500d86b
Andrew <atklenner@gmail.com>
2022-06-04 15:51:13 -0700

    add basic db functions

--- CRUD-tutorial/server.js
+++ CRUD-tutorial/server.js
@@ -8,20 +8,32 @@ MongoClient.connect(process.env.CONNECTION_STRING)
   .then((client) => {
     console.log("Connected to Database");
     const db = client.db("star-wars-quotes");
+    const quotesCollection = db.collection("quotes");
+
+    app.get("/", (req, res) => {
+      quotesCollection
+        .find()
+        .toArray()
+        .then((results) => {
+          console.log(results);
+        })
+        .catch((err) => console.error(err));
+      res.sendFile(__dirname + "/index.html");
+    });
+
+    app.post("/quotes", (req, res) => {
+      quotesCollection
+        .insertOne(req.body)
+        .then((result) => {
+          res.redirect("/");
+        })
+        .catch((err) => console.error(err));
+    });
   })
   .catch((err) => {
     console.error(err);
   });
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
-
-app.post("/quotes", (req, res) => {
-  console.log(req.body);
-  console.log("submit");
-});
 
 app.listen(3000, () => console.log("listening on port 3000"));
