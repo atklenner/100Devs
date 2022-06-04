282b92fb78fbb1dc3d9c23c38472f6a194e21b12
Andrew <atklenner@gmail.com>
2022-06-04 12:13:39 -0700

    finish get req

--- CRUD-tutorial/server.js
+++ CRUD-tutorial/server.js
@@ -1,2 +1,12 @@
 const express = require("express");
 const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/quotes", (req, res) => {
+  console.log("submit");
+});
+
+app.listen(3000, () => console.log("listening on port 3000"));
