6b2f281aed230d4ea7bf5a9719ffdfd5b622598c
Andrew Klenner <atklenner@gmail.com>
2022-10-12 20:34:19 -0700

    finish day 4

---
+++ huntober/arrays/arrays-4.js
@@ -0,0 +1,55 @@
+let myGrid = [
+  ["a", "b", "c"],
+  ["d", "e", "f"],
+  ["g", "h", "i"],
+];
+
+// call move up function with 'h' and myGrid
+// console.log(myGrid) = [['a', 'b', 'c'], ['d', 'h', 'f'], ['g', 'e', 'i']]
+
+// call move up function again, same arguments
+// console.log(myGrid) = [['a', 'h', 'c'], ['d', 'b', 'f'], ['g', 'e', 'i']]
+
+// call move up function again, same arguments
+// Note that 'h' is already as far up as it can go
+// console.log(myGrid) = [['a', 'h', 'c'], ['d', 'b', 'f'], ['g', 'e', 'i']]
+
+// call move down function this time, with 'f' and myGrid as arguments
+// console.log(myGrid) = [['a', 'h', 'c'], ['d', 'b', 'i'], ['g', 'e', 'f']]
+
+// call move down function again, same arguments
+// Note that 'f' is already as far down as it can go
+// console.log(myGrid) = [['a', 'h', 'c'], ['d', 'b', 'i'], ['g', 'e', 'f']]
+
+function moveUp(value, arr) {
+  arr.forEach((row, index) => {
+    if (row.includes(value) && index >= 1) {
+      let column = row.indexOf(value);
+      let valueToBeSwapped = arr[index - 1][column];
+      arr[index][column] = valueToBeSwapped;
+      arr[index - 1][column] = value;
+    }
+  });
+}
+
+function moveDown(value, arr) {
+  arr.forEach((row, index) => {
+    if (row.includes(value) && index < arr.length - 1) {
+      let column = row.indexOf(value);
+      let valueToBeSwapped = arr[index + 1][column];
+      arr[index][column] = valueToBeSwapped;
+      arr[index + 1][column] = value;
+    }
+  });
+}
+
+moveUp("h", myGrid);
+console.log(myGrid);
+moveUp("h", myGrid);
+console.log(myGrid);
+moveUp("h", myGrid);
+console.log(myGrid);
+moveDown("f", myGrid);
+console.log(myGrid);
+moveDown("f", myGrid);
+console.log(myGrid);
