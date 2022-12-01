7ab89b042104df7a2fa3bbb832c0fd2fa06f3fe2
Andrew Klenner <atklenner@gmail.com>
2022-11-30 18:24:21 -0800

    finished

---
+++ huntober/standups/day18.js
@@ -0,0 +1,18 @@
+// You will be given an array of strings and your task is to remove all consecutive duplicate letters from each string in the array.
+
+// For example:
+
+// dup(["abracadabra","allottee","assessee"]) = ["abracadabra","alote","asese"].
+// dup(["kelless","keenness"]) = ["keles","kenes"].
+// Strings will be lowercase only, no spaces 
+
+// dup(["ccooddddddewwwaaaaarrrrsssss","piccaninny","hubbubbubboo"]),['codewars','picaniny','hubububo'])
+// dup(["abracadabra","allottee","assessee"]),['abracadabra','alote','asese'])
+// dup(["kelless","keenness"]), ['keles','kenes'])
+
+function dup(s) {
+  // map each string in the array
+  // split each string
+  // filter the array
+  return s.map((string) => string.split("").filter((char, index, str) => index === 0 ? true : char !== str[index - 1]).join(""));
+}
